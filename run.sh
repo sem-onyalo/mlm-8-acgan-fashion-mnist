3e1befe81e5b08b4c197656db1e05ba46eae2a2f
Sem Onyalo <sem.onyalo@slalom.com>
2021-11-11 20:02:04 -0500

    bash script for automated training

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e # exit when any command fails
+
+wrkdir="."
+venvcmd="env/Scripts/activate"
+runcmd="python main.py --summary"
+
+log()
+{
+    echo [$(date)] "$1"
+}
+
+while getopts "w:v:r:" args; do
+  case $args in
+    w) wrkdir=$OPTARG;;
+    v) venvcmd=$OPTARG;;
+    r) runcmd=$OPTARG;;
+  esac
+done
+
+log "Switching to working directory '$wrkdir'"
+cd $wrkdir
+
+log "Activating virtual environment '$venvcmd'"
+source $venvcmd
+
+log "Running '$runcmd'"
+$runcmd
