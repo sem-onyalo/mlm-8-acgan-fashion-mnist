62b2d60597a158a722146487c77b0f99933e7de1
Sem Onyalo <sem.onyalo@slalom.com>
2021-11-15 18:46:15 -0500

    added all installation and run commands

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 set -e # exit when any command fails
 
 wrkdir="."
-venvcmd="env/bin/activate"
+venvcmd="python3 -m venv env"
+activate="env/bin/activate"
+install="pip3 install -r requirements.txt"
 runcmd="python3 main.py --summary"
 
 log()
@@ -11,10 +13,12 @@ log()
     echo [$(date)] "$1"
 }
 
-while getopts "w:v:r:" args; do
+while getopts "w:v:a:i:r:" args; do
   case $args in
     w) wrkdir=$OPTARG;;
     v) venvcmd=$OPTARG;;
+    a) activate=$OPTARG;;
+    i) install=$OPTARG;;
     r) runcmd=$OPTARG;;
   esac
 done
@@ -22,8 +26,14 @@ done
 log "Switching to working directory '$wrkdir'"
 cd $wrkdir
 
-log "Activating virtual environment '$venvcmd'"
-source $venvcmd
+log "Creating virtual environment '$venvcmd'"
+$venvcmd
+
+log "Activating virtual environment '$activate'"
+source $activate
+
+log "Installing requirements '$install'"
+$install
 
 log "Running '$runcmd'"
 $runcmd
